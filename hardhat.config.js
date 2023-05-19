53f6e67534fe37c4dcf01677850956ef5a382ac3
mihir108 <mihirkumarsrivastava@gmail.com>
2023-05-20 00:20:09 +0530

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  // networks: {
+  //   polygon_mumbai: {
+  //     chainId: 80001,
+  //     url: "https://polygon-mumbai.g.alchemy.com/v2/0awa485pp03Dww2fTjrSCg7yHlZECw-K",
+  //     // url: process.env.POLYGON_MUMBAI,
+  //     accounts: [
+  //       `0x${"f2211d726b37710b750fa80da41f73172853fa2ac82181aca2ff4233e3c6ce9f"}`,
+  //     ],
+  //   },
+  // },
+};
